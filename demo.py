3dbc68e1e7526dfc24ec59101078855825f403f2
Kevin Zombro <kevzombro@gmail.com>
2018-11-27 20:08:18 -0700

    sine wave plots

---
+++ demo.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 18:25:34 2018
+
+@author: kevzo
+"""
+
+import numpy as np
+from matplotlib import pyplot
+
+
+def scatter(i, j):
+    x = np.linspace(-np.pi, np.pi, num=i)
+    y = np.array([np.sin(j*x) for j in range(j)]).sum(axis=0)
+    y = np.clip(y, -1, 1)
+    pyplot.scatter(x, y, s=.2)
+
+
+def plot():
+    i = 20000
+    plots = 20
+    j = np.arange(1, plots)
+    for k in j:
+        pyplot.subplot(plots, 1, k)
+        scatter(i, k)
+    pyplot.show()
+
+
+plot()
